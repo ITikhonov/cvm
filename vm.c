b0ef5526a0cdca1daf01b994bb8270f9e787653e
Ivan Tikhonov <kefeer@brokestream.com>
2012-07-07 02:08:22 +0600

    reentrable

--- vm.c
+++ vm.c
@@ -9,84 +9,71 @@
 #define B (4)
 #define END (5)
 
-uint16_t code[1024]={};
+static uint16_t tos(uint16_t *code) { return code[code[SP]]; }
+static uint16_t pop(uint16_t *code) { return code[code[SP]--]; }
+static void push(uint16_t *code, uint16_t v) { code[SP]++; code[code[SP]]=v; }
 
-int init_code(int datasize) {
-	code[IP]=5+8*2+datasize;
-	code[SP]=5-1;
-	code[RP]=5+8-1;
-	return code[IP];
-}
+static uint16_t rpop(uint16_t *code) { return code[code[RP]--]; }
+static void rpush(uint16_t *code,uint16_t v) { code[RP]++; code[code[RP]]=v; }
 
-static uint16_t tos() { return code[code[SP]]; }
-static uint16_t pop() { return code[code[SP]--]; }
-static void push(uint16_t v) { code[SP]++; code[code[SP]]=v; }
+static uint16_t readop(uint16_t *code) { return code[code[IP]++]; }
 
-static uint16_t rpop() { return code[code[RP]--]; }
-static void rpush(uint16_t v) { code[RP]++; code[code[RP]]=v; }
-
-static uint16_t readop() { return code[code[IP]++]; }
-
-void step() {
-		uint8_t op=readop();
+void step(uint16_t *c) {
+		uint8_t op=readop(c);
 		switch(op) {
 		// Reference; http://www.colorforth.com/inst.htm
-		case 0x00: code[IP]=rpop(); break;
-		case 0x01: { uint16_t r=rpop(); rpush(code[IP]); code[IP]=r; } break;
-		case 0x02: code[IP]=readop(); break;
-		case 0x03: rpush(code[IP]); code[IP]=readop(); break;
+		case 0x00: c[IP]=rpop(c); break;
+		case 0x01: { uint16_t r=rpop(c); rpush(c,c[IP]); c[IP]=r; } break;
+		case 0x02: c[IP]=readop(c); break;
+		case 0x03: rpush(c,c[IP]); c[IP]=readop(c); break;
 		case 0x04: // we don't have slots so no unext, sorry
-		case 0x05: if(code[RP]) { code[IP]=readop(); } else { readop(); } break;
-		case 0x06: if(!tos()) { code[IP]=readop(); } else { readop(); } break;
-		case 0x07: if(tos()>0) { code[IP]=readop(); } else { readop(); } break;
+		case 0x05: if(c[RP]) { c[IP]=readop(c); } else { readop(c); } break;
+		case 0x06: if(!tos(c)) { c[IP]=readop(c); } else { readop(c); } break;
+		case 0x07: if(tos(c)>0) { c[IP]=readop(c); } else { readop(c); } break;
 
-		case 0x08: push(readop()); break;
-		case 0x09: push(code[code[A]++]); break;
-		case 0x0a: push(code[code[B]]); break;
-		case 0x0b: push(code[code[A]]); break;
+		case 0x08: push(c,readop(c)); break;
+		case 0x09: push(c,c[c[A]++]); break;
+		case 0x0a: push(c,c[c[B]]); break;
+		case 0x0b: push(c,c[c[A]]); break;
 		case 0x0c: abort(); // port??? wtf is that
-		case 0x0d: code[code[A]++]=pop(); break;
-		case 0x0e: code[code[B]]=pop(); break;
-		case 0x0f: code[code[A]]=pop(); break;
+		case 0x0d: c[c[A]++]=pop(c); break;
+		case 0x0e: c[c[B]]=pop(c); break;
+		case 0x0f: c[c[A]]=pop(c); break;
 
-		case 0x10: push(pop()*pop()); break;
-		case 0x11: push(pop()*2); break;
-		case 0x12: push(pop()/2); break;
-		case 0x13: push(-pop()); break;
-		case 0x14: push(pop()+pop()); break;
-		case 0x15: push(pop()&pop()); break;
-		case 0x16: push(pop()^pop()); break;
+		case 0x10: push(c,pop(c)*pop(c)); break;
+		case 0x11: push(c,pop(c)*2); break;
+		case 0x12: push(c,pop(c)/2); break;
+		case 0x13: push(c,-pop(c)); break;
+		case 0x14: push(c,pop(c)+pop(c)); break;
+		case 0x15: push(c,pop(c)&pop(c)); break;
+		case 0x16: push(c,pop(c)^pop(c)); break;
 
-		case 0x17: pop(); break;
-		case 0x18: push(tos()); break;
-		case 0x19: push(rpop()); break;
-		case 0x1a: push(code[code[SP]-1]); break;
-		case 0x1b: push(code[A]); break;
+		case 0x17: pop(c); break;
+		case 0x18: push(c,tos(c)); break;
+		case 0x19: push(c,rpop(c)); break;
+		case 0x1a: push(c,c[c[SP]-1]); break;
+		case 0x1b: push(c,c[A]); break;
 		case 0x1c: break;
-		case 0x1d: rpush(pop()); break;
-		case 0x1e: code[B]=pop(); break;
-		case 0x1f: code[A]=pop(); break;
+		case 0x1d: rpush(c,pop(c)); break;
+		case 0x1e: c[B]=pop(c); break;
+		case 0x1f: c[A]=pop(c); break;
 
 		case 0x98: exit(0); break;
-		case 0x99: printf("0x%02x\n",tos()); break;
+		case 0x99: printf("0x%02x\n",tos(c)); break;
 
 		default: printf("unknown op %02x\n",op);
 		}
 }
 
 int main() {
-	uint16_t i=init_code(16);
-	code[i++]=0x08;
-	code[i++]=0x123;
-	code[i++]=0x08;
-	code[i++]=0x1;
-	code[i++]=0x14;
-	code[i++]=0x99;
-	code[i++]=0x98;
-	
+	uint16_t code[1024];
+
+	FILE *f=fopen("test.vm","r");
+	fread(code,1,sizeof(code),f);
+	fclose(f);
 
 	for(;;) {
-		step();
+		step(code);
 	}
 	return 0;
 }
